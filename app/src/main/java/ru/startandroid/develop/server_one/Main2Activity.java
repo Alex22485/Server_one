080bd62833924e32afb791766b49acc1f439d9c5
Alex22485 <alex.224.85@mail.ru>
2019-11-26 22:48:05 +0700

    обновлено отображение иформации +

--- app/src/main/java/ru/startandroid/develop/server_one/Main2Activity.java
+++ app/src/main/java/ru/startandroid/develop/server_one/Main2Activity.java
@@ -3,13 +3,17 @@ package ru.startandroid.develop.server_one;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -19,17 +23,28 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Calendar;
 import java.util.Collections;
 import java.util.List;
 
 public class Main2Activity extends AppCompatActivity {
 
-    //Добавлено
+    //ADD ListView
     ListView lv2;
     List<String> basa=new ArrayList<String>(  );
     ArrayAdapter ad;
+    TextView txt;
 
-    String[] array={"Alex","Jon","Kate"};
+
+    // ADD Calendar
+    Button choisData;
+    int year;
+    int month;
+    int dayOfmonth;
+    Calendar calendar;
+    DatePickerDialog datePickerDialog;
+
+    String[] array={};
 
 
 
@@ -43,67 +58,96 @@ public class Main2Activity extends AppCompatActivity {
         basa=new ArrayList<String>( Arrays.asList( array ) );
         ad= new ArrayAdapter<>( this,android.R.layout.simple_list_item_1,basa );
         lv2.setAdapter( ad );
+        txt=findViewById(R.id.txt);
+
+
+        choisData=findViewById(R.id.choisData);
+        txt=findViewById(R.id.txt);
 
 
 
 
-       //  final ListView lvMain2 = ( ListView ) findViewById( R.id.lv2 );
+        choisData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                calendar= Calendar.getInstance();
+                year=calendar.get(Calendar.YEAR);
+                month=calendar.get(Calendar.MONTH);
+                dayOfmonth=calendar.get(Calendar.DAY_OF_MONTH);
+                datePickerDialog=new DatePickerDialog(Main2Activity.this,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                                txt.setText(day + " " + (month + 1) + " " + year);
+                            }
+                        }, year,month,dayOfmonth);
+                datePickerDialog.show();
+            }
+        });
 
 
-        DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();
 
-        DatabaseReference usersdRef = rootRef.child("Пользователь");
 
+
+
+
+       // DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference("Заявки").child("Дата").child("В Красноярск");
+     //   DatabaseReference usersdRef = rootRef.child( txt.getText().toString() );
+      //  ValueEventListener valueEventListener = new ValueEventListener() {
+      //      @Override
+       //     public void onDataChange(DataSnapshot dataSnapshot) {
+
+       //         for (DataSnapshot ds : dataSnapshot.getChildren()) {
+
+       //           String data = ds.child("дата").getValue(String.class);
+       //           String flight = ds.child("рейс").getValue(String.class);
+       //           String phone  = ds.child("phone").getValue(String.class);
+
+         //           Log.d("TAG", data+"  "+"Рейс номер"+""+flight+"  "+phone);
+          //          basa.add( data+"  "+"Рейс номер"+""+flight+"  "+phone );
+          //          Collections.sort(basa);
+         //           ad.notifyDataSetChanged();
+        //        }
+        //    }
+
+      //      @Override
+     //       public void onCancelled(DatabaseError databaseError) {
+      //      }
+    //    };
+        //usersdRef.addListenerForSingleValueEvent(valueEventListener);
+    }
+
+    public void next_list2(View view){
+        Intent n = new Intent(this,Main3Activity.class);
+        startActivity(n);
+    }
+
+    public void btnInsert (View view){
+        DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference("Заявки").child("Дата").child("В Красноярск");
+        DatabaseReference usersdRef = rootRef.child( txt.getText().toString() );
         ValueEventListener valueEventListener = new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
 
                 for (DataSnapshot ds : dataSnapshot.getChildren()) {
 
+                    String data = ds.child("дата").getValue(String.class);
+                    String flight = ds.child("рейс").getValue(String.class);
+                    String phone  = ds.child("phone").getValue(String.class);
 
-
-                  String data = ds.child("дата").getValue(String.class);
-                  String flight = ds.child("рейс").getValue(String.class);
-                  String phone  = ds.child("phone").getValue(String.class);
-
-
-                    Log.d("TAG", data+"  "+"Рейс номер"+flight+"  "+phone);
-                    //edit_text.setText(phone+"  "+data+"  "+"Рейс номер"+flight);
-
-
-                    basa.add( data+"  "+"Рейс номер"+flight+"  "+phone );
+                    Log.d("TAG", data+"  "+"Рейс номер"+""+flight+"  "+phone);
+                    basa.add(flight+"  "+"Рейс"+"  "+"Телефон"+"  "+phone+"  "+data );
                     Collections.sort(basa);
                     ad.notifyDataSetChanged();
-
-
-
                 }
-               // ArrayAdapter<String> adapter = new ArrayAdapter(Main2Activity.this, android.R.layout.simple_list_item_1,array);
-                //lvMain2.setAdapter(adapter);
-
-
-
             }
 
             @Override
             public void onCancelled(DatabaseError databaseError) {
-
             }
-        };
-        usersdRef.addListenerForSingleValueEvent(valueEventListener);
-    }
-
-    public void next_list2(View view){
-        Intent n = new Intent(this,Main3Activity.class);
-        startActivity(n);
-
+        };usersdRef.addListenerForSingleValueEvent(valueEventListener);
 
 
     }
-
-
-
-
-
     }
 
